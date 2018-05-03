275b114f95222d529f2cedd32fcf96b2ea33daf9
Emilio José <emilio.jose.pradera@gmail.com>
2018-05-03 17:49:56 +0200

    2 commit

--- src/packageTodo/Ejecutable.java
+++ src/packageTodo/Ejecutable.java
@@ -1,9 +1,55 @@
 package packageTodo;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ejecutable {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fichero = new FileInputStream("personas");
+		BufferedReader lector = new BufferedReader (new InputStreamReader(fichero));
+		String linea = "";
+		String tipoDato[];
+		String valorTipoDato[];
+		int contador = 0;
 		
-	}
+		PersonaBuilder per1 = new PersonaBuilder("hola");
+		
+		while ((linea = lector.readLine()) != null) {
+			tipoDato = linea.split("#");
+			if (tipoDato[contador].equals("colorOjos")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setColorOjos(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("colorPelo")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setColorPelo(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("tipoPelo")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setTipoPelo(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("tipoOjos")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setTipoOjos(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("edad")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setEdad(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("colorPelo")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setColorPelo(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("colorPelo")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setColorPelo(valorTipoDato[1]);
+			} else if (tipoDato[contador].equals("colorPelo")) {
+				valorTipoDato = tipoDato[contador].split(":");
+				per1.setColorPelo(valorTipoDato[1]);
+			}
+			
+			contador++;
+		}
+		
+		per1.build();
 
+		lector.close();
+	}
 }
--- src/packageTodo/PersonaBuilder.java
+++ src/packageTodo/PersonaBuilder.java
@@ -1,96 +1,95 @@
 package packageTodo;
 
-public class Persona {
+public class PersonaBuilder {
+	//Obligado
+	private String nombre;
+	
+	//Opcional
 	private String colorOjos;
 	private String colorPelo;
 	private String tipoPelo;
 	private String tipoOjos;
-	private String nombre;
 	private int edad;
 	private String ciudad;
 	private double peso;
 	private double altura;
 	
-	public Persona(PersonaBuilder builder) {
-		this.colorOjos = builder.getColorOjos();
-		this.colorPelo = builder.getColorPelo();
-		this.tipoPelo = builder.getTipoPelo();
-		this.tipoOjos = builder.getTipoOjos();
-		this.nombre = builder.getNombre();
-		this.edad = builder.getEdad();
-		this.ciudad = builder.getCiudad();
-		this.peso = builder.getPeso();
-		this.altura = builder.getAltura();
+	public PersonaBuilder(String nombre) {
+		this.nombre = nombre;
 	}
 	
-	public String getColorOjos() {
-		return this.colorOjos;
-	}
-	
-	public void setColorOjos(String colorOjos) {
-		this.colorOjos = colorOjos;
-	}
-	
-	public String getColorPelo() {
-		return this.colorPelo;
-	}
-	
-	public void setColorPelo(String colorPelo) {
-		this.colorPelo = colorPelo;
-	}
-	
-	public String getTipoPelo() {
-		return this.tipoPelo;
-	}
-	
-	public void setTipoPelo(String tipoPelo) {
-		this.tipoPelo = tipoPelo;
-	}
-	
-	public String getTipoOjos() {
-		return this.tipoOjos;
-	}
-	
-	public void setTipoOjos(String tipoOjos) {
-		this.tipoOjos = tipoOjos;
+	public Persona build() {
+		return new Persona(this);
 	}
 	
 	public String getNombre() {
 		return this.nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
+	public String getColorOjos() {
+		return this.colorOjos;
+	}
+
+	public void setColorOjos(String colorOjos) {
+		this.colorOjos = colorOjos;
+	}
+
+	public String getColorPelo() {
+		return this.colorPelo;
+	}
+
+	public void setColorPelo(String colorPelo) {
+		this.colorPelo = colorPelo;
+	}
+
+	public String getTipoPelo() {
+		return this.tipoPelo;
+	}
+
+	public void setTipoPelo(String tipoPelo) {
+		this.tipoPelo = tipoPelo;
+	}
+
+	public String getTipoOjos() {
+		return this.tipoOjos;
+	}
+
+	public void setTipoOjos(String tipoOjos) {
+		this.tipoOjos = tipoOjos;
+	}
+
 	public int getEdad() {
 		return this.edad;
 	}
-	
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-	
+
 	public String getCiudad() {
 		return this.ciudad;
 	}
-	
+
 	public void setCiudad(String ciudad) {
 		this.ciudad = ciudad;
 	}
-	
+
 	public double getPeso() {
 		return this.peso;
 	}
-	
+
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
-	
+
 	public double getAltura() {
 		return this.altura;
 	}
-	
+
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
